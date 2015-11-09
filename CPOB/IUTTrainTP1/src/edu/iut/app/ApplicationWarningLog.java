cf7c456e013cb89225d1fadd02da2ec90210ca71
unknown <clavos@hotmail.fr>
2015-11-09 12:12:59 +0100

    Ajout initial

---
+++ CPOB/IUTTrainTP1/src/edu/iut/app/ApplicationWarningLog.java
@@ -0,0 +1,14 @@
+package edu.iut.app;
+
+public class ApplicationWarningLog extends AbstractApplicationLog {
+
+	public ApplicationWarningLog() {
+		super();
+	}
+	
+	@Override
+	public void setMessage(String message) {
+		this.message = message;
+		super.fireMessage("[WARNING]", this.message);
+	}
+}
